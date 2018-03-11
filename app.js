f4de02880f41338b2f357c5394791df0ad6025ef
zhangjindong <monkey8d8jin@gmail.com>
2018-03-11 20:04:52 +0800

    scp to root

--- app.js
+++ app.js
@@ -26,6 +26,6 @@ app.use(serve(path.join(__dirname, 'public')));
 app.use(compress());
 
 if (!module.parent) {
-    app.listen(3000);
+    app.listen(80);
     console.log('listening on port 3000');
 }
